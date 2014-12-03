5786acde8f4593a97063628a5f9b6fd770c00ed5
Raul <amos.1790@yahoo.com>
2014-12-02 23:14:44 -0430

    volpe nos esta trolleando en esa verga, diciendo que da error

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ $_SESSION['lugar']= "index";
        <div class="row">
          <div class="col-md-2"></div>
          <div class="col-md-8">
-               <h1>Providing You with Health Care at Home.</h1> <br/>
+               <h1>Comprometidos con el desarrollo Tecnológico, Educacional y Saludable de la nación.</h1> <br/>
                 <p style="text-align:justify;text-indent:40px;">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.​</p> 
                 <p style="text-align:justify;text-indent:40px;">This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.</p>
         </div>
